58a487e2128769c4503125b83e36ccc06094eae5
Valerii Chuiko <valerachuiko@gmail.com>
2021-12-06 19:29:21 -0500

    Add first tutorial

---
+++ _build/jupyter_execute/ipynb/tutorial_1.py
@@ -0,0 +1,62 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# # GitHub classroom
+
+# All assignments will be uploaded and checked via GitHub classroom 
+# You need to register on [GitHub](https://github.com/) in order to follow this tutorial. Registration process is described [here](https://www.wikihow.com/Create-an-Account-on-GitHub)
+# 
+# When you set up your account use [this link](https://classroom.github.com/a/iZKwWPGv) to connect to github classroom
+# Ones you folow the link you'll see ![p1](pics/2.png)
+# Click `Accept the assignment` and then update the page ![p2](pics/3.png)
+# Now you can see a created repository with the task ![p3](pics/4.png)
+# You will have only one jupyter notebook (_\*.ipynb file_) that you need to complete in order to have your assignment done.
+# 
+# In the current repo this file is called **`Normalizing_eigenfunctions.ipynb`**
+
+# ## Editing file
+# 
+# There are several ways to edit .ipynb file:
+# 1. Using [Binder](https://mybinder.org/)
+# 2. Using [Jupyter Notebook](https://docs.anaconda.com/ae-notebooks/user-guide/basic-tasks/apps/jupyter/index.html) through [Anaconda](https://www.anaconda.com/products/individual) package
+# 3. Using [Google Colab](https://colab.research.google.com/)
+# 
+# In this tutorial we will cover the how to work with jupyter notebooks through google colab.
+# For doing this you need to download the .zip archive of the repo. In order to download it click `code` and `Download Zip` ![p4](pics/5.png)
+# 
+# When repo is downloaded you need to go to to [Google Colab](https://colab.research.google.com/) and add there .ipynb file from the repo: `File` $\rightarrow$ `Upload notebook` ![p5](pics/6.png)
+# So you can see this window ![p6](pics/7.png)
+# Upload **.ipynb** file 
+# 
+# Voila! ![p7](pics/8.png)
+# 
+# ### Understanding the assignment
+# There is an instruction written in the notebook. All you need to do is to fill the code between 
+# ```
+# ### START YOUR CODE HERE
+# ...
+# ### END YOUR CODE HERE
+# ```
+# ![p8](pics/9.png)
+# 
+# Ones code is completed, you can click the play button and evaluate the cell.
+# When you are satisfied with a code just click `File` $\rightarrow$ `Save` and then `Download` $\rightarrow$ `Download .ipynb`
+# ![p9](pics/10.png)
+
+# ## Submitting an assignment
+# 
+# Submiting assignment is breeze!
+# When you downloaded the final version of the file just go to your repo and click `Add file` $\rightarrow$ `Upload files` and upload newly created file ![p10](pics/11.png)
+# Now commit your changes and click `Commit changes` ![p11](pics/12.png)
+
+# ## Check the result
+# 
+# In order to check whether your code passed tests all you need to do is go to `Actions`$\rightarrow$ `Your commit` $\rightarrow$ `Test with pytest`. ![p12](pics/13.png) ![p13](pics/14.png)
+# 
+# If you can see green check mark - ***CONGRATULATIONS!*** you passed the assignment 
+# 
+# In case one wasn't lucky enough the following may occur:
+# ![p14](pics/15.png) ![p15](pics/16.png)
+# 
+# So, at this stage you may try to find a mistake and/or contact me `@RichRick1`
+# ![p16](pics/17.png) ![p17](pics/18.png)
