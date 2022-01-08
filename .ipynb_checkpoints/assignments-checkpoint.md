93379d2a57b8f0bfa3fcb5bbc732042ad34b3c77
Rick <valerachuiko@gmail.com>
2022-01-08 13:36:38 -0500

    creat GH fundamentals

--- .ipynb_checkpoints/assignments-checkpoint.md
+++ .ipynb_checkpoints/assignments-checkpoint.md
@@ -4,5 +4,6 @@ Your assignments will be turned in using GitHub Classroom. For instructions on h
 1. [Introduction to Jupyter](problems/Jupyter.md).
 2. [Basic Python](problems/python.md).
 3. [Introduction to Numpy](problems/numpy.md).
-4. [Computing Course Marks with Jupyter, Python, Numpy, and GitHub Classroom](problems/ghclassroom.md).
-5. [Introduction to QM and the Schr&ouml;dinger equation](problems/IntroQM.md)
+4. [GitHub fundamentals](ipynb/ghfundamentals.ipynb)
+5. [Computing Course Marks with Jupyter, Python, Numpy, and GitHub Classroom](problems/ghclassroom.md).
+6. [Introduction to QM and the Schr&ouml;dinger equation](problems/IntroQM.md)
---
+++ _build/jupyter_execute/ipynb/ghfundamentals.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# # GitHub classroom
+
+# All assignments will be uploaded and checked via GitHub classroom 
+# You need to register on [GitHub](https://github.com/) in order to follow this tutorial. Registration process is described [here](https://www.wikihow.com/Create-an-Account-on-GitHub)
+# 
+# When you set up your account use [this link](https://classroom.github.com/a/8DEfC8Tj) to connect to github classroom
+# Ones you folow the link you'll see ![p1](pics_ghf/1.png)
+# Click `Accept the assignment` ![p2](pics_ghf/2.png)
+# Now you can update the page and see ![p3](pics_ghf/3.png)
+# **Great job!** We have created a repository with the task ![p4](pics_ghf/4.png)
+# 
+# So far your task is to work through this repository and learn the basics of GitHub workflow.
+# See ya in the next Tutorial!
