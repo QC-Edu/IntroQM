ab51887ede064c3114bc9fb8ce325d959982b03b
Rick <valerachuiko@gmail.com>
2022-01-09 22:34:03 -0500

    build book

--- _build/jupyter_execute/ipynb/Tutorial_0.py
+++ _build/jupyter_execute/ipynb/Tutorial_0.py
@@ -4,31 +4,33 @@
 # # Python _and_ GitHub fundamentals
 # 
 # 
-# Hello Folks! In this tutorial we are going to cover basics steps of __editing__, __submiting__ and __analyzing__ our results using GitHub environment along with basics python skills.
+# Hello Folks! In this tutorial we are going to cover basics steps of __editing__, __submitting__ and __analyzing__ our results using GitHub environment along with basic python skills.
 
 # ## Receive the assignments
 # 
 # 
-# This moked assignment is available via the [link](https://classroom.github.com/a/8eHJCZ35). Accept the assignment, as described in [GitHub fundamentals](https://qc-edu.github.io/IntroQM2022/ipynb/ghfundamentals.html)
+# This mock assignment is available via the [link](https://classroom.github.com/a/8eHJCZ35). Accept the assignment, as described in [GitHub fundamentals](https://qc-edu.github.io/IntroQM2022/ipynb/ghfundamentals.html)
 # ![p1](pics_t0/1.png)
 # 
-# Now you can see a created repository with the task ![p1](pics_t0/2.png)
+# Now you will see that a new repository was created with the task ![p1](pics_t0/2.png)
 # There is only one file --jupyter notebook (_\*.ipynb file_)-- that you need to complete in order to have your assignment done. 
 # 
-# In the current repo this file is called **Schr&ouml;dinger.ipynb**
+# In the current repo this file is called **Schrodinger.ipynb**
 
-# ## Editing file
+# ## Editing a Jupyter Notebook
 # 
-# There are several ways to edit .ipynb file:
-# 1. Using [Binder](https://mybinder.org/)
-# 2. Using [Jupyter Notebook](https://docs.anaconda.com/ae-notebooks/user-guide/basic-tasks/apps/jupyter/index.html) through [Anaconda](https://www.anaconda.com/products/individual) package
-# 3. Using [Google Colab](https://colab.research.google.com/)
+# There are several ways to edit .ipynb file. 
+# 1. Using [Google Colab](https://colab.research.google.com/). **Recommended!**
+# 1. Using [Binder](https://mybinder.org/). 
+# 1. You can, in general, use any online Jupyter Hub (e.g. [syzygy](https://mcmaster.syzygy.ca/)). However, it can be difficult to ensure that the dependencies you need are installed.
+# 1. You can edit Jupyter Notebooks locally. It is relatively easy to use [jupyter](https://docs.anaconda.com/ae-notebooks/user-guide/basic-tasks/apps/jupyter/index.html) through [Anaconda](https://www.anaconda.com/products/individual). One can also use [Visual Studio Code](https://code.visualstudio.com/) to edit [notebooks](https://code.visualstudio.com/docs/datascience/jupyter-notebooks).
 # 
-# In this tutorial we will cover the how to work with jupyter notebooks through google colab.
-# For doing this you need to download the .zip archive of the repo. In order to download it click `code` and `Download Zip` ![p4](pics_t0/3.png)
+# We will ensure that assignments work with colab. We'll try to help people using other platforms, but make no promises.
 # 
-# When repo is downloaded you need to go to to [Google Colab](https://colab.research.google.com/) and add there .ipynb file from the repo: `File` $\rightarrow$ `Upload notebook` ![p5](pics_t0/4.png)
-# Upload **Schrodinger.ipynb** file 
+# In this tutorial we will cover the how to work with Jupyter notebooks through google colab. To do this, you need to download the .zip archive of the repo. In order to download it click `code` and `Download Zip` ![p4](pics_t0/3.png)
+# 
+# When the repo is downloaded you need to go to to [Google Colab](https://colab.research.google.com/) and add the .ipynb file from the repo: `File` $\rightarrow$ `Upload notebook` ![p5](pics_t0/4.png)
+# Upload the **Schrodinger.ipynb** file 
 # 
 # Voila! ![p6](pics_t0/5.png)
 # 
@@ -78,4 +80,6 @@
 # In case you can't pass the test click at the commit name (`Wrong answer` in this particular case) and then contact me `@RichRick1`at the comment section
 # ![p16](pics_t0/15.png) ![p17](pics_t0/16.png)
 # 
+# 
+
 # 
